ac1588127757f4eb34f14d1448285e7e3214497e
neutligen <neutligen@gmail.com>
2015-09-25 06:55:05 +0000

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,21 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-  test "should get home" do
+  test "homeページにアクセスできる" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Ruby on Rails Tutorial Test App"
   end
 
-  test "should get help" do
+  test "helpページにアクセスできる" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Ruby on Rails Tutorial Test App"
   end
-
+  
+  test "aboutページにアクセスできる" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Test App"
+  end
 end
