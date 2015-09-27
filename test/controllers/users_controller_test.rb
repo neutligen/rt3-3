1404fed999f9b29d9c01800d32b3278c70ec08e6
neutligen <neutligen@gmail.com>
2015-09-27 09:34:05 +0000

    Chap 5 Exercrse

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,14 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+  
+  def setup
+    @base_title = "Ruby on Rails Tutorial Test App"
+  end
+  
   test "新規登録ページにアクセスできる" do
     get :new
     assert_response :success
+    assert_select "title", "Sign up | #{@base_title}"
   end
-
 end
