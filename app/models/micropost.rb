a1176582e9036e26f82033e0337533522a68c672
neutligen <neutligen@gmail.com>
2015-10-11 07:09:09 +0000

    Add user microposts2

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,14 +5,13 @@ class Micropost < ActiveRecord::Base
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
   validate  :picture_size
-  
-  private
-  
-   # アップロード画像のサイズを検証する
-  def picture_size
-    if picture.size > 5.megabytes
-      errors.add(:picture, "画像の上限は5MBまでです")
-    end
-  end
 
+  private
+
+    # アップロード画像のサイズを検証する
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end
 end
